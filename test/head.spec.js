57cda681733aebb137a2cc23089e1580f69af7de
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-29 17:12:57 +0530

    Added another test to the head function

--- test/head.spec.js
+++ test/head.spec.js
@@ -5,4 +5,8 @@ describe('head', () => {
     it('Head of [1, 2, 3] should be 1', () => {
         expect(head([1, 2, 3])).toEqual(1);
     });
+
+    it('Head of [] should be null or undefined', () => {
+        expect(head([])).toBeUndefined();
+    });
 })
