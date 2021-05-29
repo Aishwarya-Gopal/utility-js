3a6cac373ecab6f9d6ce3cc3175d073f7d2dfe31
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-29 19:48:13 +0530

    Single number cubing test for cube function

--- test/cube.spec.js
+++ test/cube.spec.js
@@ -1,11 +1,11 @@
 const cube = require('../src/cube');
 
 describe('Cube', () => {
-  it('Cube of [1, 2, 3] should be [1, 8, 27]', () => {
-    expect(cube([1, 2, 3])).toEqual([1, 8, 27]);
+  it('Cube of 1 should be 1', () => {
+    expect(cube(1)).toEqual(1);
   });
 
-  it('Cube of [] should be []', () => {
-    expect(cube([])).toEqual([]);
+  it('Cube of 2 should be 8', () => {
+    expect(cube(2)).toEqual(8);
   });
 });
