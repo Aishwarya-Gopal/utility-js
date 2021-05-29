1e57021816939bb5f45413528d23ffd19a04969f
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-29 17:24:59 +0530

    Adding undefined test for tail

--- test/tail.spec.js
+++ test/tail.spec.js
@@ -4,5 +4,9 @@ describe( 'Tail', () => {
     it("Tail of [1, 2, 3] should be 3", () => {
         expect(tail([1, 2, 3])).toEqual(3);
     });
+
+    it("Tail of [] should be undefined",()=>{
+        expect(tail([])).toBeUndefined();
+    });
 })
 
