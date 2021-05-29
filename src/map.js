ec76c617fefb735fd679a2655dbf911965aa5279
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-29 19:48:37 +0530

    Lint fix

--- src/map.js
+++ src/map.js
@@ -1,7 +1,7 @@
 const map = (list, method) => {
-  list.forEach(function(part, index, list) {
+  list.forEach(function (part, index, list) {
     list[index] = method(list[index]);
-  })
+  });
   return list;
 };
 module.exports = map;
