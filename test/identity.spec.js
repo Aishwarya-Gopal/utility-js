db7cd846a6f18b30ba356fc45de864b13cbedb48
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-29 18:43:56 +0530

    Added the identity method

---
+++ test/identity.spec.js
@@ -0,0 +1,10 @@
+const identity = require('../src/identity');
+describe('Identity', () => {
+  it('Identity of [] should give []', () => {
+    expect(identity([])).toEqual([]);
+  });
+
+  it('Identity of [1, 2, 3] should give [1, 2, 3]', () => {
+    expect(identity([1, 2, 3])).toEqual([1, 2, 3]);
+  });
+});
